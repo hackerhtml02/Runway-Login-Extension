578eff7ea4ae970b862c903a02bd5e6c19a436a0
hackerhtml02 <97086315+hackerhtml02@users.noreply.github.com>
2025-01-30 18:39:29 +0500

    Update content.js

--- content.js
+++ content.js
@@ -1,35 +1,17 @@
-setTimeout(() => {
-    let button = document.evaluate(
-        "//*[@id='root']/div/div/div[1]/div[3]/button[1]", 
-        document, 
-        null, 
-        XPathResult.FIRST_ORDERED_NODE_TYPE, 
-        null
-    ).singleNodeValue;
+var button = document.evaluate("//*[@id='root']/div/div[4]/div[1]/div/div[2]/div/div[2]/div/div[2]/button[1]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+
+if (button) {
+    button.click();
+} else {
+    console.error("Button not found!");
+}
+setTimeout(function() {
+    var button = document.evaluate("/html/body/div[5]/div/section/div[2]/div[3]/button", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 
     if (button) {
         button.click();
         console.log("Button clicked successfully!");
     } else {
-        console.log("Button not found!");
+        console.error("Button not found!");
     }
-}, 3000);
-
-setTimeout(function() {
-    var element = document.evaluate('//*[@id="root"]/div/div[2]/button/div', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-    if (element) {
-        element.click();
-    } else {
-        console.log("Element not found");
-    }
-}, 5000);
-
-setTimeout(function() {
-    var element = document.evaluate('//*[text()="Start creating"]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-    if (element) {
-        element.click();
-    } else {
-        console.log("Element not found");
-    }
-}, 10000);
-
+}, 2000); // 2000 milliseconds = 2 seconds
